ecc7533e7433d88d70434f4c5c897d7c2a8acfaf
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-08-17 15:11:58 -0400

    edited metadata and added has haddress to mint-nft

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -4,3 +4,10 @@ const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const web3 = createAlchemyWeb3(API_URL);
 const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
 console.log(JSON.stringify(contract.abi));
+
+
+const contractAddress = "0xd3247e21fb606b8d39199d5be92bba9e9b951a25";
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
+
+
+
